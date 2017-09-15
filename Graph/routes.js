5b98dff25be9fb6106567921fb099b7c4aa21d26
David <davidsilva3290@gmail.com>
2017-09-14 22:28:28 -0500

    Parseo de json en routesjs

--- Graph/routes.js
+++ Graph/routes.js
@@ -2,7 +2,13 @@ module.exports = function (app) {
 
     //POST
     addUsuario = function (req, res) {
-        res.send(req.body.pedido);//manejar el pedido, este es un string
+
+var JsonObject= JSON.parse(req.body.pedido);
+console.log(JsonObject.tipo);
+console.log(JsonObject.prod.nombre);
+// console.log(req.body.pedido);
+
+        res.send("asd");
     };
 
     //API Routes Usuarios
